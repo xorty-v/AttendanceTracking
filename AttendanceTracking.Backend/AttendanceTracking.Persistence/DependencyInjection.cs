a222b5c715930345f551ce8d8d24bc57f95dabaa
Ramazi <gvindzhiliya.ramazi@mail.ru>
2023-03-19 03:21:29 +0400

    Added dependency injection and connection string

---
+++ AttendanceTracking.Backend/AttendanceTracking.Persistence/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using AttendanceTracking.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AttendanceTracking.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionsStrings = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<AttendanceDbContext>(options =>
+        {
+            options.UseNpgsql(connectionsStrings);
+        });
+        services.AddScoped<IAttendanceDbContext>(provider=>
+            provider.GetService<AttendanceDbContext>());
+        return services;
+    }
+}
